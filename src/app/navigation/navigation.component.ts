fb524650878cff2f9a8b48ce049b3ddf0b6a5491
Joeri Damme <joeri.damme@kpn.com>
2019-06-19 19:05:45 +0200

    feat: show login/logout button.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
+
+  public isLoggedIn(): boolean {
+    return !this.authenticationService.isTokenExpired();
+  }
 
   ngOnInit() {
   }
