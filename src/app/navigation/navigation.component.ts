55e969d4fe866ef201a6261dc58070a64590fb74
Joeri Damme <joeri.damme@kpn.com>
2019-06-19 19:45:42 +0200

    feat: logout button.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -8,10 +9,15 @@ import { AuthenticationService } from '../services/authentication.service';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService, private router: Router) { }
 
   public isLoggedIn(): boolean {
     return !this.authenticationService.isTokenExpired();
+  }
+
+  public logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 
   ngOnInit() {
