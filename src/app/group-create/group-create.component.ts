c1a59ba0679fe3196f3cae585a3d3bac11a14667
Joeri Damme <joeri.damme@kpn.com>
2019-06-20 20:32:41 +0200

    feat: add is-valid class on success.

--- src/app/group-create/group-create.component.ts
+++ src/app/group-create/group-create.component.ts
@@ -9,17 +9,44 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class GroupCreateComponent implements OnInit {
 
   createGroupForm: FormGroup;
+  submitted: boolean = false;
 
   constructor(private formBuilder: FormBuilder) { }
 
+  get f() {
+    return this.createGroupForm.controls;
+  }
+
+  getFormGroupClass(): string {
+    if (this.submitted && this.f.name.errors) {
+      return 'is-invalid';
+    }
+
+    if (this.f.name.valid) {
+      return 'is-valid';
+    }
+
+    return '';
+  }
+
   ngOnInit() {
     this.createGroupForm = this.formBuilder.group({
-      name: ['', Validators.required, Validators.minLength(6), Validators.maxLength(48)],
+      name: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(48)]],
     });
   }
 
   onSubmit() {
-    console.log('onSubmit clicked!');
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.createGroupForm.invalid) {
+      return;
+    }
+
+    console.info('Form valid!');
+    console.info({
+      value: this.createGroupForm.value,
+    });
   }
 
 }
