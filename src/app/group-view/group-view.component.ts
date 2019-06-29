c805b1f049b6569ad49a5184ded374c192223607
Joeri Damme <joeri.damme@kpn.com>
2019-06-29 11:47:20 +0200

    feat: getting group on init.

--- src/app/group-view/group-view.component.ts
+++ src/app/group-view/group-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IGroup } from '../models/group';
+import { GroupService } from '../services/group.service';
 
 @Component({
   selector: 'app-group-view',
@@ -8,13 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GroupViewComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  group: IGroup = null;
+
+  constructor(private route: ActivatedRoute, private groupService: GroupService) { }
 
   ngOnInit() {
     const uuid = this.route.snapshot.paramMap.get('uuid');
     if (!uuid) {
       // show 404
     }
+    this.groupService.get(uuid).subscribe((data: IGroup) => this.group = data);
   }
 
 }
