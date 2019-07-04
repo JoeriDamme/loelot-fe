971db8f11367e6a7c986458479731ac2485e9748
Joeri Damme <joeri.damme@kpn.com>
2019-07-04 22:23:22 +0200

    feat: added form for adding wish list item.

--- src/app/group-view/group-view.component.ts
+++ src/app/group-view/group-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IGroup } from '../models/group';
 import { GroupService } from '../services/group.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-group-view',
@@ -11,8 +12,30 @@ import { GroupService } from '../services/group.service';
 export class GroupViewComponent implements OnInit {
 
   group: IGroup = null;
+  addToWishList: FormGroup;
+  submittedWishList: boolean = false;
 
-  constructor(private route: ActivatedRoute, private groupService: GroupService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private groupService: GroupService,
+    private formBuilder: FormBuilder,
+  ) { }
+
+  get f() {
+    return this.addToWishList.controls;
+  }
+
+  getFormGroupClass(formfield: string): string {
+    if (this.submittedWishList && this.f[formfield].errors) {
+      return 'is-invalid';
+    }
+
+    if (this.f[formfield].valid) {
+      return 'is-valid';
+    }
+
+    return '';
+  }
 
   ngOnInit() {
     const uuid = this.route.snapshot.paramMap.get('uuid');
@@ -22,6 +45,16 @@ export class GroupViewComponent implements OnInit {
 
     const query = 'include=admin,creator,users';
     this.groupService.get(uuid, query).subscribe((data: IGroup) => this.group = data);
+
+    // setting up add wish list form
+    this.addToWishList = this.formBuilder.group({
+      description: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(512)]],
+    });
+  }
+
+  onSubmitWishList() {
+    this.submittedWishList = true;
+    console.log(this.addToWishList.value);
   }
 
 }
